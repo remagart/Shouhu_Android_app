366ec975a5c60a0da06e172c673cc66c3a0d7872
remagart <jfmamjjasond418@gmail.com>
2018-07-09 23:00:59 +0800

    [modify][add] 3秒跳到主頁/調整文字位置
    - 將導覽頁改成Linear Layout並且設定gravity為置中
    - 跳轉後的activity在merge後需改成主頁
    - 按上一頁不能回到導覽頁
    - 用thread sleep等待3秒
    
    20180709

--- ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/start.java
+++ ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/start.java
@@ -1,13 +1,46 @@
 package com.example.jfmamjjasond.shouhu;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import static java.lang.Thread.sleep;
 
 public class start extends AppCompatActivity {
+
+    Context thisactivity;
+    Thread t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        thisactivity = this;
+
+        t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);  //等3秒
+                    moveToHomePage();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        t1.start();
+
     }
+
+    void moveToHomePage(){
+        Intent i = new Intent();
+        //merge後要將MainActivity換成主頁的Activity
+        i.setClass(thisactivity,MainActivity.class);  //跳到主頁
+        startActivity(i);
+        finish();
+    }
+
 }
