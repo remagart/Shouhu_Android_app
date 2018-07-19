0495bd31cab02d1a65ac44979411387721795ec3
yuanjuan <juanyuan.zhang@gmail.com>
2018-07-19 09:59:54 +0800

    [Modify] 增加設定的兩個menu -toolbar.xml 增加ToolbarMENU設定中的兩個menu -MainActivity.java 增加註解與設定manu的按鍵設定 -values 的strings.xml 增加兩個字串
    20180719

--- ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/MainActivity.java
+++ ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/MainActivity.java
@@ -15,11 +15,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  implements ViewPager.OnPageChangeListener{ //實作ViewPaper 頁面轉換監聽事件
-    private BottomNavigationView navigation;
-    private ViewPager viewPager;
 
+    private BottomNavigationView navigation; //宣告下方選單
+    private ViewPager viewPager;//宣告ViewPager搭配Fragment
+    //宣告五個Fragment物件
     private Homepage homepage = new Homepage();
     private Fragment2 fragment2 = new Fragment2();
     private Fragment3 fragment3 = new Fragment3();
@@ -39,17 +41,18 @@ public class MainActivity extends AppCompatActivity  implements ViewPager.OnPage
         LayoutInflater inflater = LayoutInflater.from(this);
         View view = inflater.inflate(R.layout.bartitle,null);
         tvtitle =view.findViewById(R.id.tvtitle);
-
+/**************************************************************************************/
         //設定自訂ToolBar樣式
         android.support.v7.app.ActionBar actionBar = getSupportActionBar(); // 取得ActionBar物件
         assert actionBar != null;//如果actionBar不為空則向下執行
         actionBar.setDisplayShowTitleEnabled(false); //隱藏ToolBar左上標題
         actionBar.setLogo(R.mipmap.icon_96); //設定左上Icon
         actionBar.setDisplayUseLogoEnabled(true);//顯示LOGO(icon)
-        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);//顯示左上Icon
         actionBar.setCustomView(view); // 設置自訂layout(view)來顯示中間標題
         actionBar.setDisplayShowCustomEnabled(true);
-
+/************************************************************************************/
+        //在介面有一個 viewPager
         viewPager = findViewById(R.id.viewPager);
         //添加viewPager事件監聽
         viewPager.addOnPageChangeListener(this);
@@ -93,14 +96,14 @@ public class MainActivity extends AppCompatActivity  implements ViewPager.OnPage
             return true;
         }
     };
-
+/*******************************************************************************************************************/
+//toolBarmenu設定
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //設定Toolbar顯示
         MenuInflater inflater =getMenuInflater();
         inflater.inflate(R.menu.toolbar,menu);
         return super.onCreateOptionsMenu(menu);
-
     }
     //Toolbar_Menu設定監聽事件
     @Override
@@ -132,16 +135,19 @@ public class MainActivity extends AppCompatActivity  implements ViewPager.OnPage
                 dialogSign= builder.create();
                 dialogSign.show();
                 break;
-            case R.id.use:
+            case R.id.use: //使用說明
 
                 break;
-            case R.id.set:
+            case R.id.setnew://設定個人頁面
+                Toast.makeText(this,"setpersonal",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.setsleep://設定睡眠提醒
+                Toast.makeText(this,"setsleep",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.report://報表
 
                 break;
-            case R.id.report:
-
-                break;
-            case R.id.version:
+            case R.id.version://版本
 
                 break;
             case R.id.signout: //登出事件
@@ -167,7 +173,9 @@ public class MainActivity extends AppCompatActivity  implements ViewPager.OnPage
 
         return super.onOptionsItemSelected(item);
     }
-    //必須實作的方法1,當頁面在滑動的時候會調用此方法
+ /********************************************************************************************************/
+    //實作Fragment方法
+    //必須實作的方法1,當頁面在滑動的時候會使用此方法
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -194,7 +202,7 @@ public class MainActivity extends AppCompatActivity  implements ViewPager.OnPage
                 break;
         }
     }
-    //必須實作的方法3, 此方法是在狀態改變的時候調用
+    //必須實作的方法3, 此方法是在狀態改變的時候使用
     @Override
     public void onPageScrollStateChanged(int state) {
 
