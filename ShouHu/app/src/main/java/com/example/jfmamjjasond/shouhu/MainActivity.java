e5cba522036a479d96d22baee28c3400627c17a4
remagart <jfmamjjasond418@gmail.com>
2018-07-23 11:18:05 +0800

    [Modify] 導覽頁後可以到首頁並可切BMI
    - MainActivity.java:新增BMI頁的Fragment
    - BMI_information.java:改成android.support.v4.app.Fragment
    
    20180723

--- ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/MainActivity.java
+++ ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity  implements ViewPager.OnPage
     private Homepage homepage = new Homepage();
     private BMI_result fragment_bmi = new BMI_result();
     private sleep fragment_sleep = new sleep();
+    private BMI_information bmi_information = new BMI_information();
     private Fragment4 fragment4 = new Fragment4();
     private TimerFragment timerActivity = new TimerFragment();
 
