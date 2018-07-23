7eb9feee43ffc7db25b25646a9c8b846479c28cd
remagart <jfmamjjasond418@gmail.com>
2018-07-23 12:30:51 +0800

    [Add] 新增sleep頁面並設定Fragment
    - AndroidManifest.xml:建立start的activity
    - activity_sleep.xml:建立layout的草稿
    - MainActivity.java:設定Fragment可以移到sleep頁面
    - sleep.java:繼承Fragment並初始程式
    
    20180723

---
+++ ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/sleep.java
@@ -0,0 +1,25 @@
+package com.example.jfmamjjasond.shouhu;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class sleep extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_sleep, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+}
