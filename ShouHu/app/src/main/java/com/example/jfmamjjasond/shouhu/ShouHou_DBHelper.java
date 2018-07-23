8da20f8c92a1437cc5a6a946f4646169e033c4bc
remagart <jfmamjjasond418@gmail.com>
2018-07-23 15:58:30 +0800

    [Add] 新增SQlite資料庫for sleep time
    - ShouHou_DBHelper.java:建立DBHelper
    
    20180723

---
+++ ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/ShouHou_DBHelper.java
@@ -0,0 +1,44 @@
+package com.example.jfmamjjasond.shouhu;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ShouHou_DBHelper extends SQLiteOpenHelper {
+
+    final static String KEY_DB_NAME = "ShouHou_DB";
+    final static String KEY_SLEEP_TABLE_NAME = "Sleep_Table";
+    final static String KEY_ID = "_id";
+    final static String KEY_name = "user_name";
+    //昨晚睡覺時間
+    final static String KEY_SLEEP_TIME = "Sleep_Time";
+    //今早起床時間
+    final static String KEY_WAKE_TIME = "Wake_Time";
+
+    public ShouHou_DBHelper(Context c){
+        super(c,KEY_DB_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //共有id,暱稱,昨晚睡覺時間,今早睡覺時間四個欄位
+        String sql = "CREATE TABLE IF NOT EXISTS "
+                + KEY_SLEEP_TABLE_NAME
+                + " ( "
+                + KEY_ID + " INTEGER PRIMARY KEY NOT NULL,  "
+                + KEY_name + " CHAR(8) NOT NULL, "
+                + KEY_SLEEP_TIME + " DATETIME, "
+                + KEY_WAKE_TIME + " DATETIME "
+                + " ) ";
+        ;
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // 只有在更新DB版本會用到
+        String sql = "DROP TABLE IF EXISTS " + KEY_SLEEP_TABLE_NAME;
+        db.execSQL(sql);
+        onCreate(db);
+    }
+}
