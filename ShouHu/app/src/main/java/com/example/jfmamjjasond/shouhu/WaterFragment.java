b7d42b49bfd4ae07a58507d387882880e30c7b69
juanyuanzhang <40617047+juanyuanzhang@users.noreply.github.com>
2018-07-26 04:28:52 +0800

    [測試水2] 20180726

--- ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/WaterFragment.java
+++ ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/WaterFragment.java
@@ -56,153 +56,13 @@ public class WaterFragment extends android.support.v4.app.Fragment {
         int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
         date = year+"-"+month+"-"+dayOfMonth;
         thisactivity = getContext();
-//        wdbAdapter = new WDBAdapter(thisactivity);
-//        Cursor wcursorposition = wdbAdapter.querydatapostion(date);
-//        if (wcursorposition.getCount()!=0){
-//            Log.i("goin", String.valueOf(wcursorposition.getColumnCount()));
-//            Cursor wcursorchecked = wdbAdapter.querydatachecked("0");
-//            Log.i("goin2", wcursorchecked.getString(0));
-//        }
 
-//        Log.i("date=",date);
-//        thisactivity = getContext();
         wdbAdapter = new WDBAdapter(thisactivity);
-        Cursor cursor = wdbAdapter.listshow(date);
-        Log.e("黑黑","123456");
-        Log.e("YOYO",cursor.getString(0));
-        Log.e("YOYO2",cursor.getString(1));
-        Log.e("YOYO3",cursor.getString(2));
-        Log.e("YOYO4",cursor.getString(3));
-        Log.e("YOYO5",cursor.getString(4));
-        waterListCheckAdapter =new WaterListCheckAdapter(getContext(),cursor);
-        cursor = wdbAdapter.listshow(date);
-        listview.setAdapter(waterListCheckAdapter);
-        cursor.close();
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
-            {
-                public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-                {
-                    Log.i("gg", String.valueOf(listShow.get(0)));
-                    chkItem = (CheckedTextView) v.findViewById(R.id.check1);
+        // Cursor cursor = wdbAdapter.listshow(date);
 
-                    switch (position){
-                        case 0:
-                            if(!listShow.get(1) && !listShow.get(2) && !listShow.get(3)&& !listShow.get(4) && !listShow.get(5)) {
-                                listShow.set(0,false);
-                                chkItem.setChecked(!chkItem.isChecked());
-                                listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
-                                Toast.makeText(getActivity(), String.valueOf(listShow.get(0)), Toast.LENGTH_SHORT).show();
-                                //查詢是否已經有新增資料
-                                Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
-                                if(wcursor.getCount()==0) {
-                                    wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
-                                }else {
-                                    wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
-                                }
-                                Log.i("try", String.valueOf(listShow.get(0)));
-                                if(wcursor.getCount()!=0) {
-                                    Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
-                                }
-                            }
-                            break;
-                        case 1:
-                            if (listShow.get(0) && !listShow.get(2) && !listShow.get(3)&& !listShow.get(4) && !listShow.get(5) ){
-                               chkItem.setChecked(!chkItem.isChecked());
-                                listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
-                                Toast.makeText(getActivity(), String.valueOf(listShow.get(1)), Toast.LENGTH_SHORT).show();
-                                Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
-                                if(wcursor.getCount()==0) {
-                                    wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
-                                }else {
-                                    wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
-                                }
-                                Log.i("try", String.valueOf(listShow.get(0)));
-                                if(wcursor.getCount()!=0) {
-                                    Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
-                                }
-                            }
-                            break;
-                        case 2:
-                            if (listShow.get(0) &&listShow.get(1) &&!listShow.get(3)&& !listShow.get(4) && !listShow.get(5) ) {
-                                chkItem.setChecked(!chkItem.isChecked());
-                                listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
-                                Toast.makeText(getActivity(), String.valueOf(listShow.get(2)), Toast.LENGTH_SHORT).show();
-                                Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
-                                if(wcursor.getCount()==0) {
-                                    wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
-                                }else {
-                                    wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
-                                }
-                                Log.i("try", String.valueOf(listShow.get(0)));
-                                if(wcursor.getCount()!=0) {
-                                    Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
-                                }
-                            }
-                            break;
-                        case 3:
-                            if (listShow.get(0) &&listShow.get(1) && listShow.get(2) &&!listShow.get(4) && !listShow.get(5) ) {
-                                chkItem.setChecked(!chkItem.isChecked());
-                                listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
-                                Toast.makeText(getActivity(), String.valueOf(listShow.get(3)), Toast.LENGTH_SHORT).show();
-                                Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
-                                if(wcursor.getCount()==0) {
-                                    wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
-                                }else {
-                                    wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
-                                }
-                                Log.i("try", String.valueOf(listShow.get(0)));
-                                if(wcursor.getCount()!=0) {
-                                    Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
-                                }
-                            }
-                            break;
-                        case 4:
-                            if (listShow.get(0) &&listShow.get(1) && listShow.get(2) &&listShow.get(3) && !listShow.get(5) ) {
-                                chkItem.setChecked(!chkItem.isChecked());
-                                listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
-                                Toast.makeText(getActivity(), String.valueOf(listShow.get(4)), Toast.LENGTH_SHORT).show();
-                                Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
-                                if(wcursor.getCount()==0) {
-                                    wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
-                                }else {
-                                    wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
-                                }
-                                Log.i("try", String.valueOf(listShow.get(0)));
-                                if(wcursor.getCount()!=0) {
-                                    Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
-                                }
-                            }
-                            break;
-                        case  5:
-                            if(listShow.get(1) && listShow.get(2) && listShow.get(3)&& listShow.get(4) && listShow.get(0)) {
-                                chkItem.setChecked(!chkItem.isChecked());
-                                listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
-                                Toast.makeText(getActivity(), String.valueOf(listShow.get(5)), Toast.LENGTH_SHORT).show();
-                                Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
-                                if(wcursor.getCount()==0) {
-                                    wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
-                                }else {
-                                    wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
-                                }
-                                Log.i("try", String.valueOf(listShow.get(0)));
-                                if(wcursor.getCount()!=0) {
-                                    Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
-                                }
-                            }
-                            break;
-
-                    }
-                    Log.i("gg", String.valueOf(listShow.get(0)));
-
-
-
-                    if(listShow.get(position)){
-
-                        Log.i("try", String.valueOf(listShow.get(0)));
-                    }
-                }
-            }
-        );
+        // waterListCheckAdapter =new WaterListCheckAdapter(getContext(),cursor);
+        // cursor = wdbAdapter.listshow(date);
+        //listview.setAdapter(waterListCheckAdapter);
 
         listShow = new ArrayList<Boolean>();
         list = new ArrayList<String>();
@@ -220,6 +80,129 @@ public class WaterFragment extends android.support.v4.app.Fragment {
         listShow.add(false);
         listAdapter = new  WaterListitemAdapter(getActivity(),list);
         listview.setAdapter(listAdapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                                        {
+                                            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+                                            {
+                // Log.i("gg", String.valueOf(listShow.get(0)));
+                chkItem = (CheckedTextView) v.findViewById(R.id.check1);
+
+                switch (position){
+                    case 0:
+                        if(!listShow.get(1) && !listShow.get(2) && !listShow.get(3)&& !listShow.get(4) && !listShow.get(5)) {
+                            listShow.set(0,false);
+                            chkItem.setChecked(!chkItem.isChecked());
+                            listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
+                            Toast.makeText(getActivity(), String.valueOf(listShow.get(0)), Toast.LENGTH_SHORT).show();
+                            //查詢是否已經有新增資料
+                            Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
+                            if(wcursor.getCount()==0) {
+                                wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
+                            }else {
+                                wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
+                            }
+                            Log.i("try", String.valueOf(listShow.get(0)));
+                            if(wcursor.getCount()!=0) {
+                                Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
+                            }
+                        }
+                        break;
+                    case 1:
+                        if (listShow.get(0) && !listShow.get(2) && !listShow.get(3)&& !listShow.get(4) && !listShow.get(5) ){
+                            chkItem.setChecked(!chkItem.isChecked());
+                            listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
+                            Toast.makeText(getActivity(), String.valueOf(listShow.get(1)), Toast.LENGTH_SHORT).show();
+                            Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
+                            if(wcursor.getCount()==0) {
+                                wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
+                            }else {
+                                wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
+                            }
+                            Log.i("try", String.valueOf(listShow.get(0)));
+                            if(wcursor.getCount()!=0) {
+                                Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
+                            }
+                        }
+                        break;
+                    case 2:
+                        if (listShow.get(0) &&listShow.get(1) &&!listShow.get(3)&& !listShow.get(4) && !listShow.get(5) ) {
+                            chkItem.setChecked(!chkItem.isChecked());
+                            listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
+                            Toast.makeText(getActivity(), String.valueOf(listShow.get(2)), Toast.LENGTH_SHORT).show();
+                            Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
+                            if(wcursor.getCount()==0) {
+                                wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
+                            }else {
+                                wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
+                            }
+                            Log.i("try", String.valueOf(listShow.get(0)));
+                            if(wcursor.getCount()!=0) {
+                                Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
+                            }
+                        }
+                        break;
+                    case 3:
+                        if (listShow.get(0) &&listShow.get(1) && listShow.get(2) &&!listShow.get(4) && !listShow.get(5) ) {
+                            chkItem.setChecked(!chkItem.isChecked());
+                            listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
+                            Toast.makeText(getActivity(), String.valueOf(listShow.get(3)), Toast.LENGTH_SHORT).show();
+                            Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
+                            if(wcursor.getCount()==0) {
+                                wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
+                            }else {
+                                wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
+                            }
+                            Log.i("try", String.valueOf(listShow.get(0)));
+                            if(wcursor.getCount()!=0) {
+                                Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
+                            }
+                        }
+                        break;
+                    case 4:
+                        if (listShow.get(0) &&listShow.get(1) && listShow.get(2) &&listShow.get(3) && !listShow.get(5) ) {
+                            chkItem.setChecked(!chkItem.isChecked());
+                            listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
+                            Toast.makeText(getActivity(), String.valueOf(listShow.get(4)), Toast.LENGTH_SHORT).show();
+                            Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
+                            if(wcursor.getCount()==0) {
+                                wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
+                            }else {
+                                wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
+                            }
+                            Log.i("try", String.valueOf(listShow.get(0)));
+                            if(wcursor.getCount()!=0) {
+                                Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
+                            }
+                        }
+                        break;
+                    case  5:
+                        if(listShow.get(1) && listShow.get(2) && listShow.get(3)&& listShow.get(4) && listShow.get(0)) {
+                            chkItem.setChecked(!chkItem.isChecked());
+                            listShow.set(position, chkItem.isChecked());//給點選的項目設置打勾
+                            Toast.makeText(getActivity(), String.valueOf(listShow.get(5)), Toast.LENGTH_SHORT).show();
+                            Cursor wcursor = wdbAdapter.querydata(date,String.valueOf(position));
+                            if(wcursor.getCount()==0) {
+                                wdbAdapter.createdata(name,date,String.valueOf(position), String.valueOf(listShow.get(position)));//新增資料
+                            }else {
+                                wdbAdapter.updatedata(Integer.parseInt(wcursor.getString(0)), name, date, String.valueOf(position), String.valueOf(listShow.get(position)));
+                            }
+                            Log.i("try", String.valueOf(listShow.get(0)));
+                            if(wcursor.getCount()!=0) {
+                                Log.i("query", wcursor.getString(0)); //id=1 (getString(0))
+                            }
+                        }
+                        break;
+
+                }
+                Log.i("gg", String.valueOf(listShow.get(0)));
+
+                if(listShow.get(position)){
+
+                    Log.i("try", String.valueOf(listShow.get(0)));
+                }
+            }
+        }
+     );
 
     }
 
--- ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/WaterListCheckAdapter.java
+++ ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/WaterListCheckAdapter.java
@@ -15,12 +15,12 @@ public class WaterListCheckAdapter extends CursorAdapter {
     public WaterListCheckAdapter(Context context, Cursor c) {
         super(context, c,0);
         inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-        Log.e("火侯","123456");
-        Log.e("YOYO",c.getString(0));
-        Log.e("YOYO2",c.getString(1));
-        Log.e("YOYO3",c.getString(2));
-        Log.e("YOYO4",c.getString(3));
-        Log.e("YOYO5",c.getString(4));
+//        Log.e("火侯","123456");
+//        Log.e("YOYO",c.getString(0));
+//        Log.e("YOYO2",c.getString(1));
+//        Log.e("YOYO3",c.getString(2));
+//        Log.e("YOYO4",c.getString(3));
+//        Log.e("YOYO5",c.getString(4));
     }
 
     @Override
@@ -28,12 +28,14 @@ public class WaterListCheckAdapter extends CursorAdapter {
         return inflater.inflate(R.layout.water_list_item,parent,false);
     }
 
+
+
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         Log.e("Magic","123456");
         CheckedTextView chkBshow = (CheckedTextView) view.findViewById(R.id.check1);
         chkBshow.setChecked(Boolean.parseBoolean(cursor.getString(cursor.getColumnIndexOrThrow("checked"))));
-        Log.e("安安", cursor.getString(cursor.getColumnIndexOrThrow("checked")));
+//        Log.e("安安", cursor.getString(cursor.getColumnIndexOrThrow("checked")));
 
     }
 }
