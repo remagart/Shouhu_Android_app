19f97309cb93879b4c44e5f265cd25aad18f4625
juanyuanzhang <40617047+juanyuanzhang@users.noreply.github.com>
2018-07-27 02:02:31 +0800

    [modify] 新增水杯圖片
    -WaterFragment.java增加水杯圖片監聽事件
    -WLAdapter.java修改改部分程式碼
    -WDBAdapter.java修改查詢方法
    -water.xml修改介面
    -增加7個水杯圖片至mipmap
    
    20180727

--- ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/WaterFragment.java
+++ ShouHu/app/src/main/java/com/example/jfmamjjasond/shouhu/WaterFragment.java
@@ -10,7 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -25,9 +27,13 @@ public class WaterFragment extends android.support.v4.app.Fragment {
     private List<Boolean> listShow;    // 這個用來記錄哪幾個 item 是被打勾的
     private WaterListitemAdapter listAdapter;
     String ShouHu_user_name;
-    private String name ="Lulu";
+    private String name ;
     Context thisactivity;
     private WDBAdapter wdbAdapter;
+    private ImageView waterView;
+    private String date;
+    private Button btnww;
+
 
     @Nullable
     @Override
@@ -47,10 +53,24 @@ public class WaterFragment extends android.support.v4.app.Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        waterView = getView().findViewById(R.id.waterView);
+        btnww =getView().findViewById(R.id.btnww);
+        //連接資料庫
         wdbAdapter = new WDBAdapter(thisactivity);
+        get_from_activity();//取得用name
+        //取得今日日期
+        final Calendar c =Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH)+1;
+        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+        date = year+"-"+month+"-"+dayOfMonth;
+        //查詢資料
+        Cursor cursor = wdbAdapter.querydatachecked(name,date,"true");
+        setImageview(cursor);//顯使水杯現在水量
+
         listview = (ListView) getView().findViewById(R.id.listView1);
 
-        listShow = new ArrayList<Boolean>();
+        listShow = new ArrayList<Boolean>();//
         list = new ArrayList<String>();
         list.add("<500cc");
         list.add("501~1000cc");
@@ -59,11 +79,58 @@ public class WaterFragment extends android.support.v4.app.Fragment {
         list.add("2001~2500cc");
         list.add(">2500cc");
 
-        listAdapter = new  WaterListitemAdapter(getActivity(),list,listShow);
+        listAdapter = new  WaterListitemAdapter(getActivity(),list,listShow,name);
         listview.setAdapter(listAdapter);
+        //點圖片也會轉變至現在水量
+        waterView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = wdbAdapter.querydatachecked(name,date,"true");
+                setImageview(cursor);
+            }
+        });
+        //按下按鈕轉變至目前水量
+        btnww.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = wdbAdapter.querydatachecked(name,date,"true");
+                setImageview(cursor);
+            }
+        });
     }
     void get_from_activity(){
         Bundle mybundle = getArguments();
         ShouHu_user_name = mybundle.getString("user_name");
+        name = ShouHu_user_name;
+    }
+    public void setImageview(Cursor cursor){
+        if(cursor.getCount()!=0){
+            switch (Integer.valueOf(cursor.getString(cursor.getColumnIndexOrThrow("position")))){
+                case 0:
+                    waterView.setImageResource(R.mipmap.w1);
+                    break;
+                case 1:
+                    waterView.setImageResource(R.mipmap.w2);
+                    break;
+                case 2:
+                    waterView.setImageResource(R.mipmap.w3);
+                    break;
+                case 3:
+                    waterView.setImageResource(R.mipmap.w4);
+                    break;
+                case 4:
+                    waterView.setImageResource(R.mipmap.w5);
+                    break;
+                case 5:
+                    waterView.setImageResource(R.mipmap.w6);
+                    break;
+                 default:
+                     waterView.setImageResource(R.mipmap.w0);
+                     break;
+            }
+
+
+
+        }
     }
 }
